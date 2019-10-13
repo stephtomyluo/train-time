e2655cb65ba816c943195a37611d222303a8a02c
Stephanie Truong <4.struong@kscholars.org>
2019-10-12 23:48:05 -0500

    nothing works

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,28 +1,70 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyBmfnmHzvgOw7iXt724pOx2tI1ZyMwxxnM",
-    authDomain: "project-name-d2ebd.firebaseapp.com",
-    databaseURL: "https://project-name-d2ebd.firebaseio.com",
-    projectId: "project-name-d2ebd",
-    storageBucket: "project-name-d2ebd.appspot.com",
-    messagingSenderId: "149025787405",
-    appId: "1:149025787405:web:1c828b8ddc7be3ddf64f44",
-    measurementId: "G-6VK7CW4V4S"
+    apiKey: "AIzaSyBkN7hBxeg51ajiY_tcjIEUt7iikbP3GJw",
+    authDomain: "train-time-c54a0.firebaseapp.com",
+    databaseURL: "https://train-time-c54a0.firebaseio.com",
+    projectId: "train-time-c54a0",
+    storageBucket: "train-time-c54a0.appspot.com",
+    messagingSenderId: "772972568411",
+    appId: "1:772972568411:web:9db21b8ae9a8114c70c47e"
   };
 
   firebase.initializeApp(firebaseConfig);
 
 var trainDatabase = firebase.database();
 
-// Add train button, collect and store info 
-$('#add-train').on('click', function(){
-    var trainName = $('#train-name-input').val().trim();
-    var destination = $('#destination-input').val().trim();
-    var firstTrain = moment($('#time-input').val().trim(), 'HH:mm').subtract(10,'years').format('X');
-    var frequency = $('#frequency-input').val().trim();
+// Add train button, collect and store info
+$("#addTrain").on("click", function() {
+  var trainName = $("#trainNameInput")
+    .val()
+    .trim();
+  var destination = $("#destinationInput")
+    .val()
+    .trim();
+  var firstTrain = moment(
+    $("#timeInput")
+      .val()
+      .trim(),
+    "HH:mm"
+  )
+    .subtract(10, "years")
+    .format("X");
+  var frequency = $("#frequencyInput")
+    .val()
+    .trim();
 
-    console.log(firstTrain)
-})
+  console.log(firstTrain);
+});
 
-trainDatabase.ref().on('child_added', function(snapshot){
-    
-})
+// Moment.js math
+trainDatabase.ref().on("child_added", function(snapshot) {
+  var name = snapshot.val().name;
+  var destination = snapshot.val().destination;
+  var frequency = snapshot.val().frequency;
+  var firstTrain = snapshot.val().firstTrain;
+
+  var remainder = moment().diff(momemt.unix(firstTrain), "minutes") % frequency;
+  var minutes = frequency - remainder;
+  var arrival = moment()
+    .add(minutes, "m")
+    .format("hh:mm A");
+
+  console.log(remainder);
+  console.log(minutes);
+  console.log(arrival);
+
+  $("#trainTable > tBody").append(
+    "<tr><td>" +
+      name +
+      "</td><td>" +
+      destination +
+      "</td><td>" +
+      frequency +
+      "</td><td>" +
+      arrival +
+      "</td><td>" +
+      minutes +
+      "</td><td>" +
+      minutes +
+      "</td></tr>"
+  );
+});
