7beee8b108823396b42f9cf14331123e9d786713
Stephanie Truong <4.struong@kscholars.org>
2019-10-12 22:13:32 -0500

    added config

---
+++ assets/javascript/logic.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBmfnmHzvgOw7iXt724pOx2tI1ZyMwxxnM",
+    authDomain: "project-name-d2ebd.firebaseapp.com",
+    databaseURL: "https://project-name-d2ebd.firebaseio.com",
+    projectId: "project-name-d2ebd",
+    storageBucket: "project-name-d2ebd.appspot.com",
+    messagingSenderId: "149025787405",
+    appId: "1:149025787405:web:1c828b8ddc7be3ddf64f44",
+    measurementId: "G-6VK7CW4V4S"
+  };
+
+  firebase.initializeApp(firebaseConfig);
